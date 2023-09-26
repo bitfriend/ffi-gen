0394deb6de88ee7591de8bc9ba16d6d17a49dd3e
bitfriend <bitcapture@hotmail.com>
2023-09-27 05:08:49 +0800

    Remove unnecessary newline after function description

--- src/dart.rs
+++ src/dart.rs
@@ -969,7 +969,7 @@ impl DartGenerator {
     }
 
     fn generate_doc(&self, doc: &[String]) -> dart::Tokens {
-        quote!($(for line in doc => $['\n']$(static_literal("///")) $line$['\n']))
+        quote!($(for line in doc => $['\n']$(static_literal("///")) $line))
     }
 
     fn type_ident(&self, s: &str) -> String {
